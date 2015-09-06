0675426ce10aece0dfe62ed9a74a2386cf2dce47
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-09-05 20:21:57 -0400

    added --help and --version options to CLI script

--- bin/perl-to-js.pl
+++ bin/perl-to-js.pl
@@ -9,12 +9,33 @@ use lib "$0/../../lib";
 use PerlToJs;
 
 # Get input parameters
-my ($includes, $modules, $output_file) = ([], [], '');
+my ($includes, $modules, $output_file, $show_help, $show_version) = ([], [], '', 0, 0);
 GetOptions(
 	'include=s' => $includes,
 	'modules=s' => $modules,
 	'output=s' => \$output_file,
+	'help' => \$show_help,
+	'version' => \$show_version,
 );
+
+if ($show_help){
+	print "
+Usage: perl-to-js.pl [options]
+
+Options:
+  --include	specify an \@INC directory (more than one is allowed)
+  --module	specify a module to include in the bundle (at least one is required)
+  --output	specify the file to write to [default: STDOUT]
+  --help	show this screen
+  --version	show the version
+";
+	exit;
+}
+
+if ($show_version){
+	print "\nPerlToJs v$PerlToJs::VERSION\n\nCopyright (c) 2015 Matthew Francis Brunetti, Manitoulin Transport Inc., et al\n";
+	exit;
+}
 
 # Do bundle
 my $output = PerlToJs::bundle(
