5589940dde7ba6dfcc9964d99d50382894ebf46e
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-09-09 14:07:10 -0400

    Fix passing scalar/array/hash to/from subroutine; Improved simple tests; removed perl tests for dummy; removed javascript test harness

--- test/assets/tests/simple.js
+++ test/assets/tests/simple.js
@@ -1,28 +1,15 @@
-var square = perl.pkg('Dummy::Simple').sub('square');
-is(square([2]), 4);
-is(square(3), 9);
-is(square('4'), 16);
+var sum = perl.pkg('Dummy::Simple').sub('sum');
+is(sum(), 0);
+is(sum(2), 2);
+is(sum([3, 4]), 7);
+
+var encodeHash = perl.pkg('Dummy::Simple').sub('encodeHash');
+is(encodeHash({a: 1, b: 2}), 'a=1&b=2');
+is(encodeHash(['a', 1, 'b', 2]), 'a=1&b=2');
 
 var doubleEach = perl.pkg('Dummy::Simple').sub('doubleEach', {want: 'array'});
-isDeeply(doubleEach(['a', 'bb', 333, 4444]), ['aa', 'bbbb', '333333', '44444444']);
-isDeeply(doubleEach(['a', 'bb', 3333, 4444], {want: 'scalar'}), 4);
-isDeeply(doubleEach(['a', 'bb', 333, 4444], {want: 'hash'}), {'aa': 'bbbb', '333333': '44444444'});
-
-
-/* Use the test harness here? It kinda obscures the API though...
-testPackage('Dummy::Simple', function(testSub){
-	testSub('square', {}, function(testCase){
-		testCase([-3], {}, 9);
-		testCase([0], {}, 0);
-		testCase(['3'], {}, 9);
-	});
-	testSub('doubleEach', {want: 'array'}, function(testCase){
-		var args = ['a', 'bb', 333, 4444];
-		testCase(args, {}, ['aa', 'bbbb', '333333', '44444444']);
-		testCase(args, {want: 'scalar'}, 4);
-		testCase(args, {want: 'hash'}, {aa: 'bbbb', '333333': '44444444'});
-	});
-});
-*/
+isDeeply(doubleEach(['a', 'b', 3, 4]), ['aa', 'bb', '33', '44']);
+isDeeply(doubleEach(['a', 'b', 3, 4], {want: 'scalar'}), 4);
+isDeeply(doubleEach(['a', 'b', 3, 4], {want: 'hash'}), {'aa': 'bb', '33': '44'});
 
 console.log('all tests in simple.js passed!');
