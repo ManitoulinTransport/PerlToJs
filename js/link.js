2aae9534bafa6711a737034c185e56347a058a24
Death2 <downs11907@gmail.com>
2015-09-14 15:37:06 -0500

    Formating

--- js/link.js
+++ js/link.js
@@ -1,20 +1,22 @@
 var global = typeof window !== 'undefined' ? window : this;
-global._perlito = global._perlito || {bundles: []};
+global._perlito = global._perlito || {
+    bundles: []
+};
 
 global._perlito.bundles.push({
-	pkgs: p5pkg,
-	runtime: {
-		p5a_to_h: p5a_to_h,
-		p5list_to_a: p5list_to_a,
-		p5context: p5context,
-		p5HashRef: p5HashRef,
-		p5ArrayRef: p5ArrayRef,
-		p5ScalarRef: p5ScalarRef,
-		p5GlobRef: p5GlobRef,
-		p5CodeRef: p5CodeRef,
-	}
+    pkgs: p5pkg,
+    runtime: {
+        p5a_to_h: p5a_to_h,
+        p5list_to_a: p5list_to_a,
+        p5context: p5context,
+        p5HashRef: p5HashRef,
+        p5ArrayRef: p5ArrayRef,
+        p5ScalarRef: p5ScalarRef,
+        p5GlobRef: p5GlobRef,
+        p5CodeRef: p5CodeRef,
+    }
 });
 
-function write(str){
-	console.log(str);
+function write(str) {
+    console.log(str);
 }
