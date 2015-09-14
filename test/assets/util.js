0e7d739c17e6775457988526f23468a2ac356c2c
Death2 <downs11907@gmail.com>
2015-09-14 15:38:26 -0500

    Formating

--- test/assets/util.js
+++ test/assets/util.js
@@ -1,55 +1,55 @@
-function fail(message){
-	throw new Error(message);
+function fail(message) {
+    throw new Error(message);
 }
 
-function ok(is_ok, message){
-	message = message ? message + ' - ' : '';
-	if (!is_ok){
-		throw new Error(message);
-	}
+function ok(is_ok, message) {
+    message = message ? message + ' - ' : '';
+    if (!is_ok) {
+        throw new Error(message);
+    }
 }
 
-function is(actual, expected, message){
-	message = message ? message + ' - ' : '';
-	if (actual !== expected){
-		throw new Error(message + 'Expected '+ JSON.stringify(expected) + ' but got ' + JSON.stringify(actual));
-	}
+function is(actual, expected, message) {
+    message = message ? message + ' - ' : '';
+    if (actual !== expected) {
+        throw new Error(message + 'Expected ' + JSON.stringify(expected) + ' but got ' + JSON.stringify(actual));
+    }
 }
 
-function isnt(actual, unexpected, message){
-	message = message ? message + ' - ' : '';
-	if (actual === unexpected){
-		throw new Error(message + 'Expected anything but '+ JSON.stringify(unexpected));
-	}
+function isnt(actual, unexpected, message) {
+    message = message ? message + ' - ' : '';
+    if (actual === unexpected) {
+        throw new Error(message + 'Expected anything but ' + JSON.stringify(unexpected));
+    }
 }
 
-function isDeeply(actual, expected, message){
-	message = message ? message + ' - ' : '';
-	var actual_json = JSON.stringify(actual);
-	var expected_json = JSON.stringify(expected);
-	if (actual_json != expected_json){
-		throw new Error(message + 'Expected '+ expected_json + ' but got ' + actual_json);
-	}
+function isDeeply(actual, expected, message) {
+    message = message ? message + ' - ' : '';
+    var actual_json = JSON.stringify(actual);
+    var expected_json = JSON.stringify(expected);
+    if (actual_json != expected_json) {
+        throw new Error(message + 'Expected ' + expected_json + ' but got ' + actual_json);
+    }
 }
 
-function isntDeeply(actual, unexpected, message){
-	message = message ? message + ' - ' : '';
-	var actual_json = JSON.stringify(actual);
-	var unexpected_json = JSON.stringify(unexpected);
-	if (actual == unexpected){
-		throw new Error(message + 'Expected anything but '+ unexpected_json);
-	}
+function isntDeeply(actual, unexpected, message) {
+    message = message ? message + ' - ' : '';
+    var actual_json = JSON.stringify(actual);
+    var unexpected_json = JSON.stringify(unexpected);
+    if (actual == unexpected) {
+        throw new Error(message + 'Expected anything but ' + unexpected_json);
+    }
 }
 
-function throws(func, message){
-	message = message ? message + ' - ' : '';
-	var error = null;
-	try {
-		func();
-	} catch (_error){
-		error = _error;
-	}
-	if (!error){
-		throw new Error(message + 'Expected an error');
-	}
+function throws(func, message) {
+    message = message ? message + ' - ' : '';
+    var error = null;
+    try {
+        func();
+    } catch (_error) {
+        error = _error;
+    }
+    if (!error) {
+        throw new Error(message + 'Expected an error');
+    }
 }
